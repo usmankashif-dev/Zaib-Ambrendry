a7f7b7fa1992f24a9eee4ef452cf85cf5acdd279
usmankashif-dev <usmankashif711@gmail.comm>
2026-05-10 19:19:39 +0500

    Fixed errors on render security

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->web(append: [
+        $middleware->web(prepend: [
+            \App\Http\Middleware\TrustProxies::class,
+        ], append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
