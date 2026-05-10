e5e1f93538f8f5e76eb377b2dbdab4686c042fed
usmankashif-dev <usmankashif711@gmail.comm>
2026-05-10 19:58:32 +0500

    fixed etrypoijnt.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
-# Start supervisor
-/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
+echo "Starting Laravel..."
 
-# Keep container running
-tail -f /dev/null
+php artisan config:clear
+php artisan cache:clear
+php artisan route:clear
+php artisan view:clear
+
+echo "Running migrations..."
+php artisan migrate --force
+
+echo "Starting supervisor..."
+exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
